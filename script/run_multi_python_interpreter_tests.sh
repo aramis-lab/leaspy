c77b04cc94c17c0d24085cfd7d3bbedc7810957f
Benoit MARTIN <benoit.martin@icm-institute.org>
2019-09-25 15:46:22 +0200

    remove python 3.5 from tests

--- script/run_multi_python_interpreter_tests.sh
+++ script/run_multi_python_interpreter_tests.sh
@@ -12,7 +12,7 @@ done
 
 
 # setup multiple python versions
-PYTHON_VERSIONS=(3.5.7 3.6.9 3.7.4)
+PYTHON_VERSIONS=(3.6.9 3.7.4)
 for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"; do
     echo "Setting pyenv python version ${PYTHON_VERSION}";
     pyenv install -s ${PYTHON_VERSION}
