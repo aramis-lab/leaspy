4571345b6767c0e75b5e40281544412c6e6bdb59
Romain Girard <3794677-RomainGirard@users.noreply.gitlab.com>
2022-06-17 13:35:33 +0200

    [WIP] Generalize nest_parameters to dicts

--- leaspy/utils/helpers.py
+++ leaspy/utils/helpers.py
@@ -1,24 +1,34 @@
 import pandas as pd
 
-from leaspy.utils.typing import Dict, List, ParamType, Optional
+from leaspy.utils.typing import Dict, List, ParamType, Optional, Union
+from leaspy.exceptions import LeaspyTypeError
 
 
 def nest_parameters(
-    flattened_df: pd.DataFrame,
+    flattened_source: Union[pd.DataFrame, dict],
     param_names: Optional[List[ParamType]] = None,
     sep: str = "_"
 ) -> pd.DataFrame:
-    # TODO Rather a base function for dicts, convert pd.Df -> dict before use
-    nested_df = flattened_df.copy(deep=True)
+    if isinstance(flattened_source, dict):
+        is_dict = True
+        nested_result = flattened_source.copy()
+        raw_parameters = flattened_source.keys()
+    elif isinstance(flattened_source, pd.DataFrame):
+        is_dict = False
+        nested_result = flattened_source.copy(deep=True)
+        raw_parameters = flattened_source.columns.values
+    else:
+        raise LeaspyTypeError("Not implemented")
+
     nesting_plan: Dict[ParamType, int] = {}
     cols_to_test: List[ParamType] = [
-        c for c in nested_df.columns.values
+        c for c in raw_parameters
         if (param_names is None) or (c.split(sep)[0] in param_names)
     ]
 
     while len(cols_to_test) > 0:
-        for c in cols_to_test:          # e.g. "sources_1"
-            split = c.split(sep)        # e.g. ["sources", "1"]
+        for k in cols_to_test:          # e.g. "sources_1"
+            split = k.split(sep)        # e.g. ["sources", "1"]
             if len(split) > 1:
                 parent = sep.join(split[:-1])       # e.g. "sources"
                 nesting_plan[parent] = max(
@@ -26,13 +36,21 @@ def nest_parameters(
                     int(split[-1]) + 1              # Candidate list_length
                 )
 
-        for c in nesting_plan.keys():
-            children = [c + sep + str(i) for i in range(nesting_plan[c])]
-            nested_df[c] = nested_df[children].values.tolist()
-            nested_df.drop(columns=children, inplace=True)
+        for k in nesting_plan.keys():
+            children = [k + sep + str(i) for i in range(nesting_plan[k])]
+            if is_dict:
+                nested_result[k] = [
+                    list(e)
+                    for e in zip(*[nested_result[c] for c in children])
+                ]
+                for c in children:
+                    _ = nested_result.pop(c)
+            else:
+                nested_result[k] = nested_result[children].values.tolist()
+                nested_result.drop(columns=children, inplace=True)
 
         # Only newly created columns are candidates for nesting
         cols_to_test = list(nesting_plan.keys())
         nesting_plan = {}
     
-    return nested_df
+    return nested_result
