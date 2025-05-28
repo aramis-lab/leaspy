ef8c8d4be741636513fe574baa7825cc0a085b71
Gensollen <nicolas.gensollen@gmail.com>
2025-05-28 10:43:47 +0200

    Fix inheritance issues for algorithm methods `run` and `run_impl` (#402)

--- src/leaspy/models/__init__.py
+++ src/leaspy/models/__init__.py
@@ -1,3 +1,5 @@
+from typing import TypeVar
+
 from .base import BaseModel, InitializationMethod
 from .constant import ConstantModel
 from .factory import ModelName, model_factory
@@ -14,8 +16,11 @@ from .riemanian_manifold import RiemanianManifoldModel
 from .settings import ModelSettings
 from .shared_speed_logistic import SharedSpeedLogisticModel
 
+ModelType = TypeVar("ModelType", bound="BaseModel")
+
 __all__ = [
     "ModelName",
+    "ModelType",
     "InitializationMethod",
     "McmcSaemCompatibleModel",
     "RiemanianManifoldModel",
