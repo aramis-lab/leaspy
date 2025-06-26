9263ea967646037620ad610f65a4e42faaf28607
AGUILHON Lea <lea.aguilhon@icm-institute.org>
2025-06-26 16:56:18 +0200

    Changes docstring

--- src/leaspy/models/stateful.py
+++ src/leaspy/models/stateful.py
@@ -27,7 +27,21 @@ __all__ = ["StatefulModel"]
 
 
 class StatefulModel(BaseModel):
-    """Stateful models have an internal :class:`~leaspy.variables.State` to handle parameters and variables."""
+    """Stateful models have an internal :class:`~leaspy.variables.State` to handle parameters and variables.
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+    state : :class:`~leaspy.variables.State`
+        The internal state of the model, which contains the variables and their values.
+    tracked_variables : :obj:`set` [:obj:`str`]
+        Set of variable names that are tracked by the model. These variables are not necessarily part of
+        the model's state but are monitored for changes or updates. This can include variables that are
+        relevant for the model's operation but not directly stored in the state.
+
+    """
 
     def __init__(self, name: str, **kwargs):
         super().__init__(name, **kwargs)
@@ -50,12 +64,37 @@ class StatefulModel(BaseModel):
 
     @property
     def state(self) -> State:
+        """Get the internal state of the model.
+
+        Returns
+        -------
+        State : :class:`~leaspy.variables.State`
+            The internal state of the model, which contains the variables and their values.
+
+        Raises
+        ------
+        LeaspyModelInputError
+            If the model's state is not initialized yet.
+        """
         if self._state is None:
             raise LeaspyModelInputError("Model state is not initialized yet")
         return self._state
 
     @state.setter
     def state(self, s: State) -> None:
+        """Set the internal state of the model.
+        This method allows to set the internal state of the model, which is an instance of
+
+        Parameters
+        ----------
+        s : :class:`~leaspy.variables.State`
+            The new state to set for the model.
+
+        Raises
+        ------
+        LeaspyModelInputError
+            If the provided state does not match the previous state in terms of DAG structure.
+        """
         assert isinstance(s, State), "Provided state should be a valid State instance"
         if self._state is not None and s.dag is not self._state.dag:
             raise LeaspyModelInputError(
@@ -66,32 +105,80 @@ class StatefulModel(BaseModel):
 
     @property
     def dag(self) -> VariablesDAG:
+        """Get the underlying DAG of the model's state.
+
+        Returns
+        -------
+        : :class:`~leaspy.variables.dag.VariablesDAG`
+            The directed acyclic graph (DAG) representing the model's variables and their relationships
+
+        """
         return self.state.dag
 
     @property
     def hyperparameters_names(self) -> tuple[VariableName, ...]:
+        """Get the names of the model's hyperparameters.
+
+        Returns
+        -------
+        : :obj:`tuple` [:class:`~leaspy.variables.specs.VariableName`, ...]
+            A tuple containing the names of the model's hyperparameters.
+        """
         return tuple(self.dag.sorted_variables_by_type[Hyperparameter])
 
     @property
     def parameters_names(self) -> tuple[VariableName, ...]:
+        """Get the names of the model's parameters.
+
+        Returns
+        -------
+        : :obj:`tuple` [:class:`~leaspy.variables.specs.VariableName`, ...]
+            A tuple containing the names of the model's parameters.
+        """
         return tuple(self.dag.sorted_variables_by_type[ModelParameter])
 
     @property
     def population_variables_names(self) -> tuple[VariableName, ...]:
+        """Get the names of the population latent variables.
+
+        Returns
+        -------
+        : :obj:`tuple` [:class:`~leaspy.variables.specs.VariableName`, ...]
+            A tuple containing the names of the population latent variables.
+        """
         return tuple(self.dag.sorted_variables_by_type[PopulationLatentVariable])
 
     @property
     def individual_variables_names(self) -> tuple[VariableName, ...]:
+        """Get the names of the individual latent variables.
+
+        Returns
+        -------
+        : :obj:`tuple` [:class:`~leaspy.variables.specs.VariableName`, ...]
+            A tuple containing the names of the individual latent variables.
+        """
         return tuple(self.dag.sorted_variables_by_type[IndividualLatentVariable])
 
     @property
     def parameters(self) -> DictParamsTorch:
-        """Dictionary of values for model parameters."""
+        """Dictionary of values for model parameters.
+
+        Returns
+        -------
+        : :class:`~leaspy.utils.typing.DictParamsTorch`
+            A dictionary mapping parameter names to their values (as tensors).
+        """
         return {p: self._state[p] for p in self.parameters_names}
 
     @property
     def hyperparameters(self) -> DictParamsTorch:
-        """Dictionary of values for model hyperparameters."""
+        """Dictionary of values for model hyperparameters.
+
+        Returns
+        -------
+        : :class:`~leaspy.utils.typing.DictParamsTorch`
+            A dictionary mapping hyperparameter names to their values (as tensors).
+        """
         return {p: self._state[p] for p in self.hyperparameters_names}
 
     def initialize(self, dataset: Optional[Dataset] = None) -> None:
@@ -101,7 +188,7 @@ class StatefulModel(BaseModel):
 
         Parameters
         ----------
-        dataset : :class:`.Dataset`, optional
+        dataset : :class:`~leaspy.io.data.dataset.Dataset`, optional
             Input dataset from which to initialize the model.
         """
         super().initialize(dataset=dataset)
@@ -142,7 +229,7 @@ class StatefulModel(BaseModel):
 
         Parameters
         ----------
-        dataset : Dataset
+        dataset : :class:`~leaspy.io.data.dataset.Dataset`
             The dataset to use to compute initial values for the model parameters.
         """
         model_parameters_initialization = (
@@ -189,7 +276,7 @@ class StatefulModel(BaseModel):
 
         Parameters
         ----------
-        parameters : :obj:`dict` [ :obj:`str`, Any ]
+        parameters : :class:`~leaspy.utils.typing.KwargsType`]
             Contains the model's parameters.
         """
         from .utilities import val_to_tensor
@@ -257,7 +344,7 @@ class StatefulModel(BaseModel):
 
         Returns
         -------
-        NamedVariables :
+        NamedVariables : :class:`~leaspy.variables.specs.NamedVariables`
             The specifications of the model's variables.
         """
         raise NotImplementedError()
@@ -266,7 +353,18 @@ class StatefulModel(BaseModel):
     def _compute_initial_values_for_model_parameters(
         self, dataset: Dataset
     ) -> VariableNameToValueMapping:
-        """Compute initial values for model parameters."""
+        """Compute initial values for model parameters.
+
+        Parameters
+        ----------
+        dataset : :class:`~leaspy.io.data.dataset.Dataset`
+            The dataset to use to compute initial values for the model parameters.
+
+        Returns
+        -------
+        : :class:`~leaspy.utils.typing.Any`
+            A dictionary mapping variable names to their initial values.
+        """
         raise NotImplementedError()
 
     def move_to_device(self, device: torch.device) -> None:
@@ -274,7 +372,8 @@ class StatefulModel(BaseModel):
 
         Parameters
         ----------
-        device : :class:`torch.device`
+        device : :obj:`torch.device`
+            The device to which the model and its attributes should be moved.
         """
         if self._state is None:
             return
