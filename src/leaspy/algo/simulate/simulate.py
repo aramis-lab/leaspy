0c797baab48b2003d5c18fb7ce0cad00bd047f3b
lea-aguilhon <lea.aguilhon@icm-institute.org>
2025-07-21 14:52:32 +0200

    change delta

--- src/leaspy/algo/simulate/simulate.py
+++ src/leaspy/algo/simulate/simulate.py
@@ -69,13 +69,13 @@ class SimulationAlgorithm(BaseSimulationAlgorithm):
             - 'time_follow_up_std': :obj:`float`
                 Standard deviation of the follow-up TIME.
             - 'distance_visit_mean': :obj:`float`
-                Mean of distance_visits: mean time delta between two visits.
+                Mean of distance_visits: mean time spacing between two visits.
             - 'distance_visit_std': :obj:`float`
-                Standard deviation of distance_visits: std time delta between two visits.
-                Time delta between 2 visits is drawn in a normal distribution N(distance_visit_mean, distance_visit_std),
+                Standard deviation of distance_visits: std time spacing between two visits.
+                Time pacing between 2 visits is drawn in a normal distribution N(distance_visit_mean, distance_visit_std),
                 thus setting distance_visit_std to 0 enable to simulate regularly spaced visits.
             - 'min_spacing_between_visits': :obj:`float`
-                Minimum delta between visits. This delta has to be in the same unit as the TIME column.
+                Minimum spacing between two visits. This spacing has to be in the same unit as the TIME column.
                 If two visits are closer than this value, the second visit will be removed from the dataset.
                 This is used to avoid too close visits in the simulated dataset.
                 Default is 1/365 (1 day).
