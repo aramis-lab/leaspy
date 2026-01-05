f754fe08dc10dd3fecf71ce98a43ccfcbb94bb30
Sebastian <mendezpsebastian@gmail.com>
2026-01-05 11:22:41 +0100

    fixing favicon error display

--- docs/conf.py
+++ docs/conf.py
@@ -156,6 +156,9 @@ html_css_files = [
 ]
 html_js_files = ["custom.js"]
 
+# Favicon
+html_favicon = "_static/favicon.png"
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further. For a list of options available for each theme, see the
 # documentation.
