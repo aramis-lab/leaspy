48736a0858e6132edbd381967a249615c14a781f
Gabrielle Casimiro <gabrielle.casimiro@gmail.com>
2026-02-27 16:40:38 +0100

    Add files and code for Schiratti's definition of the logistic model

--- src/leaspy/models/__init__.py
+++ src/leaspy/models/__init__.py
@@ -5,20 +5,24 @@ from .joint import JointModel
 from .linear import LinearModel
 from .lme import LMEModel
 from .logistic import LogisticModel
+from .logistic_Schiratti import LogisticModelSchiratti
 from .mcmc_saem_compatible import McmcSaemCompatibleModel
+from .mixture import LogisticMultivariateMixtureModel
 from .riemanian_manifold import RiemanianManifoldModel
+from .riemanian_manifold_Schiratti import RiemanianManifoldModelSchiratti
 from .settings import ModelSettings
 from .shared_speed_logistic import SharedSpeedLogisticModel
 from .stateful import StatefulModel
 from .stateless import StatelessModel
 from .time_reparametrized import TimeReparametrizedModel
-from .mixture import LogisticMultivariateMixtureModel
+from .time_reparametrized_Schiratti import TimeReparametrizedModelSchiratti
 
 __all__ = [
     "ModelInterface",
     "ModelName",
     "McmcSaemCompatibleModel",
     "TimeReparametrizedModel",
+    "TimeReparametrizedModelSchiratti",
     "BaseModel",
     "ConstantModel",
     "StatelessModel",
@@ -27,7 +31,9 @@ __all__ = [
     "model_factory",
     "ModelSettings",
     "RiemanianManifoldModel",
+    "RiemanianManifoldModelSchiratti",
     "LogisticModel",
+    "LogisticModelSchiratti",
     "LinearModel",
     "SharedSpeedLogisticModel",
     "JointModel",
