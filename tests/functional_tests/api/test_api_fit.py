27ce9bc88b846bbedf7107605392cf40b4e27fcb
Sebastian <mendezpsebastian@gmail.com>
2026-04-03 17:48:53 +0200

    Fixing Ubuntu compatibility issue

--- tests/functional_tests/api/test_api_fit.py
+++ tests/functional_tests/api/test_api_fit.py
@@ -150,6 +150,7 @@ class LeaspyFitTestMixin(MatplotlibTestCase):
 # some noticeable reproducibility errors btw MacOS and Linux here...
 ALLCLOSE_CUSTOM = dict(
     nll_regul_ind_sum=dict(atol=5),
+    nll_regul_pop_sum=dict(atol=10),
     nll_attach=dict(atol=10),
     nll_tot=dict(atol=15),
     tau_mean=dict(atol=0.2),
